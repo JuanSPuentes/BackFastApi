d3bcccf0eb516bf49810f1f19fe74dd94370a1c0
JuanSPuentes <puentesjs03@gmail.com>
2024-05-16 15:29:05 -0500

    feat: Add user authentication functionality

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status 
 from database import SessionLocal
-from models import User
+from models import User, UserType
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
@@ -43,7 +43,21 @@ def get_db():
     finally:
         db.close()
 
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials')
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        user_type: UserType = payload.get("user_type")
+        if username is None or user_id is None:
+            raise credentials_exception
+        return {'username': username, 'user_id': user_id, 'user_type': user_type}
+    except JWTError:
+        raise credentials_exception
+    
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 async def create_user(request: CreateUserRequest, db: db_dependency):
@@ -70,15 +84,12 @@ def create_access_token(username: str, user_id: int, user_type: str, expires_del
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate credentials')
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-        user_id: int = payload.get('id')
-        user_type: UserType = payload.get("user_type")
-        if username is None or user_id is None:
-            raise credentials_exception
-        return {'username': username, 'user_id': user_id, 'user_type': user_type}
-    except JWTError:
-        raise credentials_exception
+def get_current_active_admin(current_user: user_dependency):
+    if current_user['user_type'] != UserType.admin.value:
+        raise HTTPException(status_code=400, detail="Not an admin")
+    return current_user
+
+def get_current_active_user(current_user: user_dependency):
+    if current_user['user_type'] != UserType.user.value:
+        raise HTTPException(status_code=400, detail="Not an user")
+    return current_user
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, status, Depends, HTTPException
 import models
-import auth
 from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
-from auth import get_current_user
+from auth import get_current_user, get_current_active_user, router
 
 app = FastAPI()
-app.include_router(auth.router)
+app.include_router(router)
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -21,12 +20,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
-def get_current_active_admin(current_user: user_dependency):
-    if current_user['user_type'] != models.UserType.admin.value:
-        raise HTTPException(status_code=400, detail="Not an admin")
-    return current_user
-
-@app.get('/', status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_active_admin)])
+@app.get('/', status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_active_user)])
 async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication empty')
