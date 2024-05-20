e5fa5b03b9e0ed47cb7bbb7de756eb76ca8e654c
JuanSPuentes <puentesjs903@gmail.com>
2024-05-19 19:16:30 -0500

    refactor: Update imports and rename files for better organization

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine('sqlite:///database.db', connect_args={'check_same_thread': False})
 
